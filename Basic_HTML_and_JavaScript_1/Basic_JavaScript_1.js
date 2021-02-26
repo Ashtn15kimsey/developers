9a3b583b904730cbda6859c2615d66c7ca8b8d8d
ashtn15kimsey <ashtn15@gmail.com>
2021-02-25 16:16:53 -0800

    update

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,12 @@
+
+
+
+alert("Hello, World!");
+function my_First_Function() {
+     var string="Kiss me, I'm Irish!";
+     var reslult=String.fontcolor="Black";
+     document.getElementById("Irish").innerHTML=
+     reslult;
+ }
+	 
+
